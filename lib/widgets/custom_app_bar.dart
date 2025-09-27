a1838073f661430ddae0cd2e182ce13170749d59
Melissa Solórzano <163376726+kmmelissat@users.noreply.github.com>
2025-09-27 11:05:21 -0600

    🎨 Update CustomAppBar to support dark mode: integrated ThemeProvider for dynamic SVG asset loading based on theme preference.

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
+import '../providers/theme_provider.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CustomAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return AppBar(
-      title: SvgPicture.asset('assets/images/vendly.svg', height: 50),
+      title: SvgPicture.asset(
+        themeProvider.isDarkMode
+            ? 'assets/images/vendly-dark.svg'
+            : 'assets/images/vendly.svg',
+        height: 50,
+      ),
     );
   }
 
